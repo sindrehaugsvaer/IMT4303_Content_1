7ad41a19c47d9610b39fd6c7bbd3bdc74eb47a3a
sindrehaugsvaer <sindre.haugsvaer@gmail.com>
2021-09-02 00:35:22 +0200

    div knoting

--- main.py
+++ main.py
@@ -1,18 +1,68 @@
-# This is a sample Python script.
+from nltk.tokenize import word_tokenize
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Retrieved from geeksforgeeks.org: https://www.geeksforgeeks.org/create-inverted-index-for-file-using-python/
+
+# this will open the file
+file = open('text1.txt', encoding='utf8')
+read = file.read()
+file.seek(0)
+print(read)
+print("---")
+
+# number of lines
+line = 1
+for word in read:
+    if word == '\n':
+        line += 1
+print("---")
+print("Number of lines in file is: ", line)
+print("---")
+
+# create a list to
+# store each line as
+# an element of list
+array = []
+for i in range(line):
+    array.append(file.readline())
+
+print("each line as element of list")
+print(array)
+print("---")
+
+# Remove punctuation:
+punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+for ele in read:
+    if ele in punc:
+        read = read.replace(ele, " ")
+print(read)
+print("---")
+
+# to maintain uniformity
+read = read.lower()
+print(read)
+print("---")
+
+# Creating array with all words
+oneline = read.replace('\r', '').replace('\n', '')
+print("--- oneline")
+print(oneline)
+
+allwords=oneline.split(" ")
+print("--- allwords")
+print(allwords)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#Creating inverted index:
+dict = {}
+for i in range(line):
+    check = array[i].lower()
+    for item in allwords:
 
+        if item in check:
+            if item not in dict:
+                dict[item] = []
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-print("Hello pyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            if item in dict:
+                dict[item].append(i + 1)
+print("--- inverted index!")
+print(dict)
